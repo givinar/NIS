e9ff5fb0558d2a7f95aa124db4aaa07c8e96bdaa
Alexander <alexander.a.lapshin@gmail.com>
2022-12-13 13:44:06 +0300

    Add MAPE matric

--- utils/images_compare.py
+++ utils/images_compare.py
@@ -1,3 +1,5 @@
+import torch
+from torchmetrics import MeanAbsolutePercentageError
 from dataclasses import dataclass
 from collections.abc import Callable
 import os
@@ -9,6 +11,7 @@ import OpenEXR
 import argparse
 from PIL import Image
 from SSIM_PIL import compare_ssim
+from typing import Tuple
 
 """
 Module compares two sequences of images from two different folders with ground truth image
@@ -16,39 +19,26 @@ using specified metric and shows its in the plot.
 Example: -im 'image_folder_path' -gt 'ground_truth_image_path' -s 50
 """
 
+
 @dataclass
-class ImageCompare:
-    img_folder: str
+class Metrics:
     gt_image: str
-    step: int
 
     def __post_init__(self):
-        gt_tsr = self.load_exr(self.gt_image)
-        self.gt_pil_img = Image.fromarray(np.uint8(gt_tsr * 255.))
+        self.gt_tsr = self.load_exr(self.gt_image)
+        self.gt_img = Image.fromarray(np.uint8(self.gt_tsr * 255.))
 
-    @staticmethod
-    def parse_args(arg=sys.argv[1:]):
-        arg_parser = argparse.ArgumentParser(
-            description='Comparing images by specific metric')
+    def ssim(self, preds: np.ndarray) -> float:
+        preds_img = Image.fromarray(np.uint8(preds * 255.))
+        return compare_ssim(preds_img, self.gt_img)
 
-        arg_parser.add_argument('-im', '--image_folder', type=str, required=True, help='Image folder path'),
-        arg_parser.add_argument('-gt', '--image_gt', type=str, required=True, help='Ground truth image path')
-        arg_parser.add_argument('-s', '--step', type=int, required=True, help='Iteration step')
-
-        return arg_parser.parse_args(arg)
-
-    @staticmethod
-    def list_files(path, valid_exts='exr'):
-        for (rootDir, dirNames, filenames) in os.walk(path):
-            for filename in filenames:
-                ext = filename[filename.rfind("."):].lower()
-                if ext.endswith(valid_exts):
-                    image_path = os.path.join(rootDir, filename).replace(" ", "\\ ")
-                    yield image_path
+    def mape(self, preds: np.ndarray) -> float:
+        return MeanAbsolutePercentageError()(torch.from_numpy(preds).to(torch.float),
+                                             torch.from_numpy(self.gt_tsr).to(torch.float))
 
     @staticmethod
     def load_exr(path, max_channels=3):
-        path = str(path)  # convert from Path if needed
+        path = str(path)
         if not OpenEXR.isOpenExrFile(path):
             raise ValueError(f'Image {path} is not a valid OpenEXR file')
         src = OpenEXR.InputFile(path)
@@ -76,36 +66,76 @@ class ImageCompare:
         tensor = np_data.reshape((-1, size[1], size[0]))
         return tensor.transpose(1, 2, 0)
 
-    def get_ssim(self, path: str, length: int, method: Callable) -> np.ndarray:
-        ssims = np.zeros(length, float)
+
+@dataclass
+class ImageCompare:
+    img_folder: str
+    gt_image: str
+    metric_name: str
+    step: int
+
+    def __post_init__(self):
+        self.metrics = Metrics(self.gt_image)
+        self.set_metric(self.metric_name)
+
+    @staticmethod
+    def parse_args(arg=sys.argv[1:]):
+        arg_parser = argparse.ArgumentParser(
+            description='Comparing images by specific metric')
+
+        arg_parser.add_argument('-im', '--image_folder', type=str, required=True, help='Image folder path'),
+        arg_parser.add_argument('-gt', '--image_gt', type=str, required=True, help='Ground truth image path')
+        arg_parser.add_argument('-m', '--metric', type=str, required=True, help='Metric')
+        arg_parser.add_argument('-s', '--step', type=int, required=True, help='Iteration step')
+
+        return arg_parser.parse_args(arg)
+
+    @staticmethod
+    def list_files(path, valid_exts='exr'):
+        for (rootDir, dirNames, filenames) in os.walk(path):
+            for filename in filenames:
+                ext = filename[filename.rfind("."):].lower()
+                if ext.endswith(valid_exts):
+                    image_path = os.path.join(rootDir, filename).replace(" ", "\\ ")
+                    yield image_path
+
+    def calc_metric(self, path: str, length: int, method: Callable) -> Tuple[np.ndarray, np.ndarray]:
+        metric = np.zeros(length, float)
         indices = np.zeros(length, int)
         for i, name in enumerate(self.list_files(path)):
-            tsr = self.load_exr(name)
-            pil_img = Image.fromarray(np.uint8(tsr * 255))
-            ssims[i] = method(pil_img, self.gt_pil_img)
+            metric[i] = method(self.metrics.load_exr(name))
             indices[i] = i * self.step
 
-        return indices, ssims
+        return indices, metric
 
     def show_plot(self):
         colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-        plt.figure('SSIM')
+        plt.figure(self.metric_name)
         try:
             for i, folder in enumerate(os.listdir(self.img_folder)):
                 full_path = os.path.join(self.img_folder, folder)
                 length = len(os.listdir(full_path))
-                indices, ssims = self.get_ssim(full_path, length, compare_ssim)
+                indices, ssims = self.calc_metric(full_path, length, self.metric)
                 plt.plot(indices, ssims, colors[i], label=os.path.basename(full_path))
         except IndexError:
             print('Color index error, extend color list...')
             raise SystemExit
 
         plt.xlabel('Iterations')
-        plt.ylabel('SSIM')
+        plt.ylabel(self.metric_name)
         #plt.ylim(0., 1.)
         plt.grid()
         plt.legend()
         plt.show()
+
+    def set_metric(self, opt):
+        if opt == 'SSIM':
+            self.metric = self.metrics.ssim
+        elif opt == 'MAPE':
+            self.metric = self.metrics.mape
+        else:
+            print('Unknown metric type -', opt)
+            raise SystemExit
 
 
 if __name__ == '__main__':
@@ -113,5 +143,7 @@ if __name__ == '__main__':
 
     ic = ImageCompare(options.image_folder,
                       options.image_gt,
+                      options.metric,
                       options.step)
+
     ic.show_plot()
